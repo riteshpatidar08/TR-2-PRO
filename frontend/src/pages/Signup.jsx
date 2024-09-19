7863965b019011281e53aff4d49b43369ad7932f
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-19 10:36:24 +0530

    updated Code

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 function Signup() {
+    console.log(useForm())
+   const {handleSubmit, register} = useForm()
+
+
+   const onSubmit = (data) => {
+    console.log(data)
+}
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor='Name'>Name</label>
+        <input type='text' {...register('name')}  />
+        <label htmlFor='Name'>Email</label>
+        <input type='email' {...register('email')}  />
+        <label htmlFor='Name'>phone</label>
+        <input type='text' {...register('phone')}  />
+        <label htmlFor='Name'>Name</label>
+        <input type='password' {...register('password')}  />
+        <button type='submit'>Signup</button>
+      </form>
     </div>
   )
 }
