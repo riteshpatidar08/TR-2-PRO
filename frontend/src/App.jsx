a03ef1c4664a321444347b11bae7d1cd1cac59e8
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-20 10:03:11 +0530

    updatecode

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import Signup from "./pages/Signup" 
 import {Routes , Route} from 'react-router-dom'
-
+import {Toaster} from 'sonner'
 export default function App() {
   return (
+    <div>
    <Routes>
     <Route path="/register" element={<Signup/>} />
    </Routes>
+   <Toaster position="bottom-right" />
+   </div>
   )
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-
+import axios from 'axios'
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
+import { toast } from 'sonner';
 
 const schema = z.object({
     name : z.string().min(1, "Name is Required").max(40, "Name can not exceed 40 characters") ,
     email : z.string().email("Email is requied"),
     phone : z.string().min(10 , "Phone is required") ,
     password : z.string().min(8, "Password is too short").regex(/[0-9]/,"Password must contain a Number").regex(/[a-z]/ , "Password must have one lowercase").regex(/[A-Z]/,
-    "Password must contain uppercase").regex(/[/W]/,"Password must contain a speacial symbol")
+    "Password must contain uppercase").regex(/[\w_]/,"Password must contain a speacial symbol")
 
 
 }) 
@@ -22,9 +23,18 @@ function Signup() {
 
   console.log(errors)
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+   try {
+     // console.log(data);
+    const res = await axios.post('http://localhost:3000/api/register',data)
+    console.log(res)
+    toast.success("User successfully registered")
+   } catch (error) {
+    console.log(error)
+    toast.error(error.response.data.message)
+   }
   };
+
 
   return (
     <div className="flex justify-center items-center min-h-screen">
@@ -67,7 +77,7 @@ function Signup() {
           />
           {errors.password && <p className='text-red-500 font-semibold'>{errors.password.message}</p>}
           <button
-            className="bg-blue-500 px-8 py-2 rounded-md w-full mt-3 text-white"
+            className="bg-black px-8 py-2 rounded-md w-full mt-3 text-white"
             type="submit"
           >
             Signup
