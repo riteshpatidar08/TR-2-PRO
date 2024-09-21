875b0ce0451c3fb149e00684831288108557e387
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-21 10:34:51 +0530

    updatedcode

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,47 @@
+//signup 
+import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk
+ } from "@reduxjs/toolkit"
+import axios from "axios"
+
+export const Signup = createAsyncThunk('/user/signup', async(data,{rejectWithValue})=>{
+    try {
+      const res = axios.post('http://localhost:3000/api/register')
+      console.log(res.data)
+      return res.data
+  }
+     catch (error) {
+        rejectWithValue(error)
+    }
+
+} )
+
+const initialState = {
+loading : false ,
+error : null
+}
+
+const userSlice = createSlice({
+    name : "user",
+    initialState,
+    extraReducers : (builder) => {
+ builder.addCase(Signup.pending, (state)=>{
+    state.loading = true ;
+    state.error = null
+ }).addCase(Signup.fulfilled , (state)=>{
+    state.loading = false ;
+    state.error = null
+ }).addCase(Signup.rejected,(state,action)=>{
+    state.loading = false ;
+    state.error = action.payload
+ })
+    }
+})
+
+export default userSlice.reducer
+
+//async thunk => we can use this middleware to implement all the asynchronous logic in the redux like api calling and then handle the states of the api in the slice like pending , failed , or fulfilled
+
+//NOTE using createAsyncThunk we can implement the api logic which returns a promise and handle the promise states(pending,fulfilled , failed) in userSlice.
+
+//NOTE createAsyncThunk accpets a action type and a function in which we are going to call the api.
