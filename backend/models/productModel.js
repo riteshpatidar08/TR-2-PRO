ea1da05bee88fa39f52a67dc96b1d3674c908177
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-24 10:50:37 +0530

    updatedCode

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,10 +1,35 @@
 const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema({
+ name : {
+    type : String,
+    required : [true, "Name is required"]
+ },
+ price : {
+    type : Number,
+    required : true
+ },
+ image : {
+    type : String,
+    
+ },
+ description : {
+    type : String
+ },
+ stock : {
+    type :Number,
+    default : 0
+ },
+ discountPercantage : {
+    type :Number
+ }
 
 
 })
 
+const Product = mongoose.model('Product',productSchema) ;
+
+module.exports = Product
 
 
     // name  ,
