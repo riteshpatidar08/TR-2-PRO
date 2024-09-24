e8b4d880710234731afa691f127793742fc43f6a
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-24 11:22:54 +0530

    updatedCode

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-
+import Hero from '../components/Hero'
 function Homepage() {
   return (
     <div>
    <Navbar/>
+   <Hero/>
     </div>
   )
 }
