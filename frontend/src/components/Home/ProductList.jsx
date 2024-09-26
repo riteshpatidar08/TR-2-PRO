91f3f5c847d3604f154fa701d4aa109de1685d49
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-26 09:19:54 +0530

    updatedCode

---
+++ frontend/src/components/Home/ProductList.jsx
@@ -0,0 +1,24 @@
+// import React from 'react';
+// import all_products from '../../assets/all_product';
+// import ProductCard from './ProductCard';
+// import {useSelector} from 'react-redux';
+
+// function ProductList() {
+  
+//   const {products} = useSelector((state)=>state.product)
+//   const threeProducts = products.slice(0, 3);
+//   return (
+//     <div className=" container mx-auto py-8">
+//       <h1 className="text-center mb-12 text-2xl font-normal leading-wide">
+//         JUST FOR YOU
+//       </h1>
+//       <div className="grid grid-cols-3 gap-6">
+//         {threeProducts.map((product) => (
+//           <ProductCard product={product} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ProductList;
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
+import Navbar from '../components/Home/Navbar'
+import Hero from '../components/Home/Hero'
+import Banner from '../components/Home/Banner'
+import GridSection from '../components/Home/GridSection'
 function Homepage() {
   return (
     <div>
    <Navbar/>
    <Hero/>
+   <GridSection/>
+   <Banner/>
     </div>
   )
 }
