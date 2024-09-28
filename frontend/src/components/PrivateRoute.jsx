b8835ed6e9e6b82d18762b9d4d9adf63ca7a4ad9
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-09-28 11:34:32 +0530

    upatecode till cart

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -14,7 +14,7 @@ function PrivateRoute({allowrole}) {
  }
  return  (
     <>
-   
+
     <Outlet/></>
     
  )
--- frontend/src/redux/localStorage.js
+++ frontend/src/redux/localStorage.js
@@ -1,19 +1,22 @@
 export const saveState = (state) => {
-    console.log(state)
-    try {
-        const data = JSON.stringify(state)
-        localStorage.setItem("cart" ,data)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  console.log(state);
+  try {
+    const data = JSON.stringify(state);
+    localStorage.setItem('cart', data);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export const loadState = () => {
-    try {
-        const cartData = localStorage.getItem('cart')
-        const data = JSON.parse(cartData)
-       return data
-    } catch (error) {
-        console.log(error)
+  try {
+    const cartData = localStorage.getItem('cart');
+    if (cartData === null) {
+      return undefined;
+    } else {
+      return JSON.parse(cartData);
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
