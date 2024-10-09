6c002aaf846d515aa2ef0aae8e408541ab438651
Ritesh Patidar <itsmeriteshpatidar@gmail.com>
2024-10-09 11:48:56 +0530

    updated code

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,14 +1,22 @@
 const multer = require('multer')
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2
+require('dotenv').config()
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-' + uniqueSuffix)
-  }
+cloudinary.config({
+cloud_name :  `${process.env.CLOUD_NAME}`,
+api_key :   `${process.env.CLOUDINARY_APIKEY}`,
+api_secret:   `${process.env.CLOUDINARY_APISECRET}`,
 })
+
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'product',
+    allowed_format : ['png','jpg','jpeg']
+  },
+});
 
 const upload = multer({ storage: storage })
 
